cc1fab253fdff8f9b4aa644b65ec7c45d80fdacc
Tomazzo Antonioli <tomazzo10@hotmail.com>
2022-01-14 19:57:28 +0100

    cleaning

--- router.js
+++ router.js
@@ -23,15 +23,3 @@ router.put("/articles/:id", modifyArticle);
 router.delete("/articles/:id", deleteArticle);
 
 export default router;
-
-// [GET]/:  Retornar um Status: 200 e uma Mensagem "Back-end Challenge 2021 🏅 - Space Flight News"
-
-// [GET]/articles/:   Listar todos os artigos da base de dados, utilizar o sistema de paginação para não sobrecarregar a REQUEST
-
-// [GET]/articles/{id}: Obter a informação somente de um artigo
-
-// [POST]/articles/: Adicionar um novo artigo
-
-// [PUT]/articles/{id}: Atualizar um artigo baseado no id
-
-// [DELETE]/articles/{id}: Remover um artigo baseado no id
